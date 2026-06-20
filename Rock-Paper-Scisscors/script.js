0581884025642b069d47a3a2ff2d1a11a03fb026
Saad <saadhunter007@gmail.com>
2026-06-20 17:54:17 +0500

    LITTLE CHANGE

--- Rock-Paper-Scisscors/script.js
+++ Rock-Paper-Scisscors/script.js
@@ -1,68 +1,81 @@
-let imges = document.querySelectorAll('.img');
+let imges = document.querySelectorAll(".img");
 let score = document.querySelector(".score");
-let resetBtn = document.querySelector("button")
+let resetBtn = document.querySelector("button");
 let playerScore = 0;
 let computerScore = 0;
 let result = document.querySelector("#Result");
 const arr = ["rock", "paper", "scissors"];
 
 const computer = () => {
-    return arr[Math.floor(Math.random() * 3)]
+  return arr[Math.floor(Math.random() * 3)];
+};
 
-}
-const timestamp=()=>{
-return setTimeout(()=>{
+const timestamp = () => {
+  return setTimeout(() => {
     result.innerText = "";
-},1000)
+    enable();
+  }, 700  );
+};
+
+const disable=()=>{
+    imges.forEach((img) => {
+        img.style.pointerEvents = "none";
+    });
+}
+
+const enable=()=>{
+    imges.forEach((img) => {
+        img.style.pointerEvents = "auto";
+    });
 }
 
 const Reset = () => {
-    playerScore = 0;
-    computerScore = 0;
-    score.children[0].firstChild.nodeValue = 0;
-    score.children[1].firstChild.nodeValue = 0;
-    result.innerText = "";
-    console.clear();
+  playerScore = 0;
+  computerScore = 0;
+  score.children[0].firstChild.nodeValue = 0;
+  score.children[1].firstChild.nodeValue = 0;
+  result.innerText = "";
+  console.clear();
 };
 
 const Score = () => {
-    score.children[0].firstChild.nodeValue = playerScore;
-    score.children[1].firstChild.nodeValue = computerScore;
+  score.children[0].firstChild.nodeValue = playerScore;
+  score.children[1].firstChild.nodeValue = computerScore;
 };
 
 const draw = () => {
-    result.innerText = "Draw"
-    timestamp();
-    console.log(`No Winner`)
-}
+  result.innerText = "Draw";
+  timestamp();
+  console.log(`No Winner`);
+};
 
 const Winner = (a) => {
-    result.innerText = `Winner is ${a}`
-    timestamp();
-}
+  result.innerText = `Winner is ${a}`;
+  timestamp();
+};
 
+imges.forEach((img) => {
+  img.addEventListener("click", () => {
+    disable();
+    let playerChoice = img.getAttribute("name");
+    let computerChoice = computer();
 
-imges.forEach(img => {
-    img.addEventListener("click", () => {
-        let playerChoice = img.getAttribute("name");
-        let computerChoice = computer();
-        
-        if (playerChoice === computerChoice) {
-            draw();
-        }
-        else if ((playerChoice == "rock" && computerChoice == "scissors") ||
-                 (playerChoice == "paper" && computerChoice == "rock") ||
-                 (playerChoice == "scissors" && computerChoice == "paper")) {
-            Winner("You");
-            playerScore++;
-            Score();
-        }
-        else {
-            Winner("Computer");
-            computerScore++;
-            Score();
-        }
-    });
+    if (playerChoice === computerChoice) {
+      draw();
+    } else if (
+      (playerChoice == "rock" && computerChoice == "scissors") ||
+      (playerChoice == "paper" && computerChoice == "rock") ||
+      (playerChoice == "scissors" && computerChoice == "paper")
+    ) {
+      Winner("You");
+      playerScore++;
+      Score();
+    } else {
+      Winner("Computer");
+      computerScore++;
+      Score();
+    }
+  });
 });
 
-resetBtn.addEventListener("click",Reset)
+resetBtn.addEventListener("click", Reset);
